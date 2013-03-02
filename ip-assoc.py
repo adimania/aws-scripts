a77d68bbd374f030be548a3422a8427e5179dd56
Aditya Patawari <aditya@adityapatawari.com>
2013-03-02 15:40:55 +0530

    option to choose region for ip association

--- ip-assoc.py
+++ ip-assoc.py
@@ -1,4 +1,5 @@
 import boto
+import boto.ec2
 import ConfigParser
 import os.path
 import sys
@@ -8,8 +9,6 @@ from optparse import OptionParser
 check = os.path.isfile(os.path.expanduser('~/.aws.conf'))
 if cmp(check,False) == 0:
     print Description
-    sys.exit(2)
-system_execution = 0
 config = ConfigParser.ConfigParser()
 config.read(os.path.expanduser('~/.aws.conf'))
 id = config.get("AWS", "consumer_key", raw=True)
@@ -19,12 +18,15 @@ parser = OptionParser()
 parser.add_option("-i", "--instance", dest="instance", help="Instance id of the instance.", metavar="INSTANCE_ID")
 parser.add_option("-a", "--associate", dest="assoc", help="IP address to associate.", metavar="IP_ADDR")
 parser.add_option("-d", "--disassociate", dest="disassoc", help="IP address to disassociate.", metavar="IP_ADDR")
+parser.add_option("-r", "--region", dest="region", help="Region to be connected", metavar="REGION_NAME")
 (options, args) = parser.parse_args()
 
-conn = boto.connect_ec2(id,key)
+region_obj = boto.ec2.get_region(options.region,aws_access_key_id=id,aws_secret_access_key=key)
+
+conn = boto.connect_ec2(aws_access_key_id=id,aws_secret_access_key=key,region=region_obj)
 
 if options.assoc:
   conn.associate_address(instance_id=options.instance, public_ip=options.assoc)
 elif options.disassoc:
-  conn.disassociate_address(public_ip=options.disassoc)  
+  conn.disassociate_address(public_ip=options.disassoc)
 
